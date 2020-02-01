b0aee24ccb361b1db684dae55113392758a583d9
Adam Parker <amichaelparker@gmail.com>
2020-02-01 12:48:34 -0600

    added stuffs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ConvertText = () => {
+  const [inputValue, setInputValue] = React.useState("");
+  const [alteredText, setAlteredText] = React.useState([]);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+  const alterText = (event) => {
+    if (event.key === 'Enter') {
+      const splitText = inputValue.split('');
+      setAlteredText(splitText.map((letter, index) => {
+        if (index % 5 === 0 && letter.match(/^[A-Za-z]+$/)) {
+          return <span className="altered-text" key={index}>{letter}</span>;
+        } else {
+          return <span className="normal-text" key={index}>{letter}</span>
+        }
+      }));
+    }
+  };
+
+  return (
+    <div className='container'>
+      <div className='altered-text-div'>
+        {alteredText}
+      </div>
+      <div className='textarea-container'>
+        <textarea className='textarea' value={inputValue} onChange={(e) => setInputValue(e.currentTarget.value)} onKeyDown={alterText}/>
+      </div>
+    </div>
+  )
+};
+
+ReactDOM.render(<ConvertText />, document.getElementById('root'));
 serviceWorker.unregister();
